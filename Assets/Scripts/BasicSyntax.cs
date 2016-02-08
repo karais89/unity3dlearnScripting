719d5236b4f59255342e4ba0ff010a7d920d5ae5
heojihun <karais89@gmail.com>
2016-02-08 16:33:41 +0900

    -exam3 commit

---
+++ Assets/Scripts/BasicSyntax.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace exam_03
+{
+    public class BasicSyntax : MonoBehaviour 
+    {        
+        // Use this for initialization
+        void Start () 
+        {
+            // This line is there to tell me the X Postion of my object
+            
+            /*
+            Hi there!
+            This is Two lines!
+            */
+            
+            Debug.Log(transform.position.x);
+            
+            /*
+            if(transform.position.y <= 5f)
+            {
+                Debug.Log("I`m about to hit the ground!");
+            }
+            */
+        }
+    }    
+}
