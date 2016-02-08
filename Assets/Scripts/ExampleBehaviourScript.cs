723735a3f496c82ea95d975ac7b44ec85ba16073
heojihun <karais89@gmail.com>
2016-02-08 16:25:33 +0900

    - vsCode 추가 - exam1, 2 추가

---
+++ Assets/Scripts/ExampleBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Exam_01 {
+	public class ExampleBehaviourScript : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if(Input.GetKeyDown(KeyCode.R))
+			{
+				gameObject.GetComponent<Renderer>().material.color = Color.red;
+			}
+			if(Input.GetKeyDown(KeyCode.G))
+			{
+				gameObject.GetComponent<Renderer>().material.color = Color.green;
+			}
+			if(Input.GetKeyDown(KeyCode.B))
+			{
+				gameObject.GetComponent<Renderer>().material.color = Color.blue;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace exam_02 {
+	public class VariablesAndFunctions : MonoBehaviour {
+
+		int myInt = 5;
+
+		void Start() {
+			myInt = MultiplyByTwo(myInt);
+            Debug.Log(myInt);
+		}
+        
+        // 함수 선언
+        int MultiplyByTwo(int number)
+        {
+            int ret;
+            
+            ret = number * 2;
+            
+            return ret;        
+        }
+	}
+}
