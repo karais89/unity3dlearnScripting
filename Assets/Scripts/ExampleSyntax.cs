1c9a334886967c87c84f866e5969e811b43c9e5f
heojihun <karais89@gmail.com>
2016-02-09 15:25:44 +0900

    chapter4

---
+++ Assets/Scripts/ExampleSyntax.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace exam_4
+{
+    public class ExampleSyntax : MonoBehaviour {
+
+    int myInt = 5;
+    
+    int MyFunction (int number)
+    {
+        int ret = myInt * number;
+        return ret;
+    }
+    }    
+}
