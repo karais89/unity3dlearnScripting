c513ad3c65e96bfa649621594cc440ea2c7e5ec1
heojihun <karais89@gmail.com>
2016-02-10 21:10:01 +0900

    exam11

---
+++ Assets/Scripts/EnableComponents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Exam_11
+{    
+    public class EnableComponents : MonoBehaviour 
+    {
+        private Light myLight;
+        
+        void Start()
+        {
+            myLight = GetComponent<Light>();
+        }
+        
+        void Update()
+        {
+            if(Input.GetKeyUp(KeyCode.Space))
+            {
+                myLight.enabled = !myLight.enabled;
+            }
+        }
+
+    }
+}
