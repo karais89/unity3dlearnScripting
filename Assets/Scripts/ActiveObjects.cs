5a7c44a52d12f41f8cc32f176abbe9feed029b18
heojihun <karais89@gmail.com>
2016-02-10 21:36:01 +0900

    exam12

---
+++ Assets/Scripts/ActiveObjects.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Exam_12
+{
+    public class ActiveObjects : MonoBehaviour 
+    {
+        void Start ()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/CheckState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Exam_12
+{
+    public class CheckState : MonoBehaviour 
+    {
+        public GameObject myObject;        
+        
+        void Start ()
+        {
+            Debug.Log("Active Self: " + myObject.activeSelf);
+            Debug.Log("Active in Hierarchy: " + myObject.activeInHierarchy);
+        }
+    }
+}
