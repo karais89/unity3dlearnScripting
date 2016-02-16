0d60edeae4f6ea345a9d11781595270c59c4e606
heojihun <karais89@gmail.com>
2016-02-17 00:18:10 +0900

    - 13 Translate and Rotate - 14 Look At

---
+++ Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;using System.Collections;namespace Exam_14
+{
+    public class CameraLookAt : MonoBehaviour
+    {
+        public Transform target;
+
+        void Update()
+        {
+            transform.LookAt(target);
+        }
+    }
+}
