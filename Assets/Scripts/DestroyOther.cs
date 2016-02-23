965a8f0fbfcb8832a1d03594d64621f66b469670
heojihun <karais89@gmail.com>
2016-02-23 20:25:58 +0900

    -destroy commit

---
+++ Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOther : MonoBehaviour 
+{
+	public GameObject other;
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(other);
+        }
+    }
+}
